653799e9e030f0bc43b2cb4ae1346f3a9685d783
MuntahaChowdhury <muntaha0108@gmail.com>
2026-07-23 01:18:10 -0400

    Add: FirebaseAuth Singleton

--- app/src/main/java/com/example/b07demosummer2024/MainActivity.java
+++ app/src/main/java/com/example/b07demosummer2024/MainActivity.java
@@ -2,18 +2,11 @@ package com.example.b07demosummer2024;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.widget.Button;
-import android.view.View;
-import android.content.Intent;
-
+import com.example.b07demosummer2024.auth.LoginFragment;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
