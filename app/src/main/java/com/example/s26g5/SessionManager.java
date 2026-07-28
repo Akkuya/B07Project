51767f770fae34e137d79b6160d8b7f54df0cb80
Rishi <satyajitdatta03@gmail.com>
2026-07-28 19:17:05 -0400

    feat: Filled in the constructor and destructor for the SessionManager object

--- app/src/main/java/com/example/s26g5/SessionManager.java
+++ app/src/main/java/com/example/s26g5/SessionManager.java
@@ -6,16 +6,21 @@ public class SessionManager {
     private String username;
     private boolean isAdmin;
 
+    // Singleton Design Pattern
     public static SessionManager getInstance() {
         if (instance == null) instance = new SessionManager();
         return instance;
     }
 
     public void setSession(String uid, String username, boolean isAdmin) {
-        ;
+        this.uid = uid;
+        this.username = username;
+        this.isAdmin = isAdmin;
     }
     public void clearSession() {
-        ;
+        this.uid = null;
+        this.username = null;
+        this.isAdmin = false;
     }
     public boolean isLoggedIn() { return uid != null; }
     public boolean isAdmin() { return isAdmin; }
