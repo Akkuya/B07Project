2580174ec1ef4a62e9f198caec216e39e86d4ac4
nyaakss <nyascottak@gmail.com>
2026-08-04 10:25:58 -0400

    Add: EditItemFragment.java, fragment_edit_item.xml

---
+++ app/src/main/java/com/example/s26g5/EditItemFragment.java
@@ -0,0 +1,70 @@
+package com.example.s26g5;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class EditItemFragment {
+    private EditText artifactName;
+    private Spinner spinnerCategory;
+    private Button buttonDelete;
+    private Button buttonBack;
+    private EditText lotNum;
+
+    private FirebaseDatabase db;
+    private DatabaseReference itemsRef;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_edit_item, container, false);
+
+        spinnerCategory = view.findViewById(R.id.spinnerCategory);
+        buttonDelete = view.findViewById(R.id.buttonDelete);
+        buttonBack = view.findViewById(R.id.buttonBack_e);
+        lotNum = view.findViewById(R.id.lotNumber_d);
+        artifactName = view.findViewById(R.id.itemName_d);
+
+        db = FirebaseDatabase.getInstance();
+
+        // Set up the spinner with categories
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.categories_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerCategory.setAdapter(adapter);
+
+        buttonDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                deleteItemByTitle();
+            }
+        });
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new ManageItemsFragment());
+            }
+        });
+
+        return view;
+    }
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+}
