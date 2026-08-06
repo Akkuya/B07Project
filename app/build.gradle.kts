2a22a7d5794040b55b680ed165e1139c552f2cf8
Mak3na-1 <meagan.g.maina@gmail.com>
2026-08-06 01:23:34 -0400

    fix: Saves artifacts to firebase and refers to local storage when offline

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,6 +32,7 @@ android {
 dependencies {
 
     implementation(libs.appcompat)
+    implementation(libs.google.firebase.auth)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
